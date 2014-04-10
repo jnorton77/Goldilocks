ecce93b4422f55515698a22f22a0da09cb7d6bb1
Devin Wieland <devinwieland@gmail.com>
2014-04-10 08:31:52 -0500

    adds chart.js after group mentoring session

--- Goldilocks/db/seeds.rb
+++ Goldilocks/db/seeds.rb
@@ -19,8 +19,8 @@
 
 # require 'faker'
 
-rand(100).times { Response.create(answer: "panic") }
-rand(100).times { Response.create(answer: "panic edge") }
-rand(100).times { Response.create(answer: "learning") }
-rand(100).times { Response.create(answer: "comfort edge") }
-rand(100).times { Response.create(answer: "comfort") }
+rand(100).times { Response.create(answer: "panic", user_id: 1) }
+rand(100).times { Response.create(answer: "panic edge", user_id: 1) }
+rand(100).times { Response.create(answer: "learning", user_id: 1) }
+rand(100).times { Response.create(answer: "comfort edge", user_id: 1) }
+rand(100).times { Response.create(answer: "comfort", user_id: 1) }
