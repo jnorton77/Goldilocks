8fa736f5280e45ce0fd3396b5e5beefb2f3da44f
Dan-and-John <dbrown14@gmail.com>
2014-04-12 17:33:37 -0500

    js files organized a bit; began integrating time fields

--- app/assets/javascripts/d3barchart.js
+++ app/assets/javascripts/d3barchart.js
@@ -1,72 +1,66 @@
-Array.prototype.max = function() {
-  var maximum = this[0];
-  var length = this.length;
-  for (var i = 1; i < length; i++) if (this[i] > maximum) maximum = this[i];
-  return maximum;
+<script src="d3helper.js"></script>
+
+function responseData() {
+  this.times = []
+  this.answers = []
 }
 
-Array.prototype.min = function() {
-  var minimum = this[0];
-  var len = this.length;
-  for (var i = 1; i < length; i++) if (this[i] < minimum) minimum = this[i];
-  return minimum;
-}
-
-var xScale = []
-var yScale = []
-
-var responseData = [{"answer": "comfort", "created_at":"2014-03-30 06:17:25"},
-          {"answer": "panic", "created_at":"2014-04-08 15:51:58"},
-          {"answer": "panic edge", "created_at":"2014-04-11 16:54:18"},
-          {"answer": "comfort edge", "created_at":"2014-03-31 07:59:18"},
-          {"answer": "comfort", "created_at":"2014-04-08 07:07:31"},
-          {"answer": "learning", "created_at":"2014-04-08 08:59:05"},
-          {"answer": "comfort", "created_at":"2014-04-04 12:44:28"},
-          {"answer": "panic edge", "created_at":"2014-04-11 04:42:04"},
-          {"answer": "learning", "created_at":"2014-04-07 05:25:39"},
+var responses = [{"answer": "comfort", "created_at":"2014-03-30 06:17:25"},
+          {"answer": "panic", "created_at":"2014-03-30 15:51:58"},
+          {"answer": "panic edge", "created_at":"2014-03-30 16:54:18"},
+          {"answer": "comfort edge", "created_at":"2014-03-30 07:59:18"},
+          {"answer": "comfort", "created_at":"2014-03-30 07:07:31"},
+          {"answer": "learning", "created_at":"2014-03-30 08:59:05"},
+          {"answer": "comfort", "created_at":"2014-03-30 12:44:28"},
+          {"answer": "panic edge", "created_at":"2014-03-30 04:42:04"},
+          {"answer": "learning", "created_at":"2014-03-30 05:25:39"},
           {"answer": "comfort", "created_at":"2014-03-30 02:09:09"},
-          {"answer": "panic edge", "created_at":"2014-04-08 23:28:33"},
-          {"answer": "comfort", "created_at":"2014-04-10 17:29:58"},
-          {"answer": "learning", "created_at":"2014-04-11 04:42:04"},
+          {"answer": "panic edge", "created_at":"2014-03-30 23:28:33"},
+          {"answer": "comfort", "created_at":"2014-03-30 17:29:58"},
+          {"answer": "learning", "created_at":"2014-03-30 04:42:04"},
           {"answer": "comfort", "created_at":"2014-03-30 09:04:12"},
 ];
 
-var jsonRectangles = [
-                      { "x": 10, "y": 10, "height": 800, "width":200, "color" : "green" },
-                      { "x": 160, "y": 40, "height": 800, "width":200, "color" : "purple" },
-                      { "x": 70, "y": 70, "height": 800, "width":200, "color" : "red" }];
 
-function extractX(element) { xScale.push(element.x) }
-function extractY(element) { yScale.push(element.y) }
+function extractAttribute(element, index, destinationArray, attributeName) { destinationArray.push(element.attributeName) }
 
-lineData.forEach(extractX);
-lineData.forEach(extractY);
+// responses.forEach(extractAttribute(element, responseData().times, created_at)
+// responses.forEach(extractAttribute(element, responseData().answers, answer);
 
-jsonRectangles.forEach(extractX);
-jsonRectangles.forEach(extractY);
 
-var max_x = xScale.max();
-var min_x = xScale.min();
-var width = max_x + 20
-var max_y = yScale.max();
-var min_y = yScale.min();
-var height = max_y + 20
+var calendarEvents = [
+                      { "title": "EE", "begin": "2014-03-30 010:00:00", "end":"2014-03-30 11:30:00" },
+                      { "title": "Coding", "begin": "2014-03-30 13:00:00", "end": "2014-03-30 17:00:00" },
+                      { "title": "Lunch", "begin": "2014-03-30 11:30:00", "end": "2014-03-30 13:00:00" }];
+
+// This is for the actual responses (the times of the first and last response), but we're probably not going to use this ...
+// var max_x = responseData().times.max();
+// var min_x = responseData().times.min();
+var max_x = 1000
+var min_x = 0
+var width = 1180
+
+
+// var max_y = yScale.max();
+// var min_y = yScale.min();
+var height = 980
 var padding = 10;
 
 var x = d3.scale.linear()
-                .range([0, max_x]);
-var y = d3.scale.linear()
-                .range([0, max_y]);
+                .domain([min_x, max_x]);
+// var y = d3.scale.linear()
+//                 .range([0, max_y]);
 
-var xAxis = d3.svg.axis()
+var xAxisLabel = d3.svg.axis()
                   .scale(x)
                   .orient("bottom")
-                  .ticks(6);
+                  .ticks(6)
+                  .tickFormat(d3.time.format('%a %d'));
 
-var yAxis = d3.svg.axis()
-                  .scale(y)
-                  .orient("left")
-                  .ticks(6);
+// var yAxisLabel = d3.svg.axis()
+//                   .scale(y)
+//                   .orient("left")
+//                   .ticks(6);
 
 $(document).ready(function() {
 
@@ -76,44 +70,39 @@ $(document).ready(function() {
                               .attr("id", "barchart");
 
   var rectangles = svg.selectAll("rect")
-                              .data(jsonRectangles)
+                              .data(calendarEvents)
                               .enter()
                               .append("rect");
 
   var rectAttributes = rectangles
-                        .attr("x", function(d) { return d.x; })
-                        .attr("y", function(d) { return d.y; })
-                        .attr("height", function(d) { return d.height; })
-                        .attr("width", function(d) { return d.width; })
-                        .style("fill", function(d) { return d.color; })
+                        .attr("x", function(calendarEvents) { return 10; })
+                        .attr("y", function(d) { return 40; })
+                        .attr("height", function(d) { return 80; })
+                        .attr("width", function(d) { return 80; })
+                        .style("fill", function(d) { return "blue"; })
                         .transition()
                         .duration(1250)
                         .style('opacity', .6);
 
   var lineFunction = d3.svg.line()
                           .x(function(d) {
-                            console.log(d.x + "x")
-                            return d.x; })
+                            console.log(d.created_at + "x")
+                            return d.created_at; })
                           .y(function(d) {
-                            console.log(d.y + "y")
-                            return d.y; })
+                            console.log(d.answer + "y")
+                            return d.answer; })
                           .interpolate("linear");
 
     svg.append("path")
-        .attr("d", lineFunction(lineData))
+        .attr("d", lineFunction(responseData))
         .attr("stroke-width", 2)
         .attr("stroke", "blue")
         .attr("fill", "none");
 
-    console.log(yScale)
-    console.log(yAxis)
-    console.log(xScale)
-    console.log(xAxis)
-
-svg.append("g")
-    .attr("class", "y-axis")
-    .attr("transform", "translate(" + padding + ",0)")
-    .call(yAxis);
+// svg.append("g")
+//     .attr("class", "y-axis")
+//     .attr("transform", "translate(" + padding + ",0)")
+//     .call(yAxis);
 
 svg.append("g")
     .attr("class", "x-axis")
