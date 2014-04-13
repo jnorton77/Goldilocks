176d0bb9e679e4f08b8e23cfa95f65d6787f74c3
Devin Wieland <devinwieland@gmail.com>
2014-04-13 13:44:17 -0500

    disabled 'like a boss' alert

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,18 +1,18 @@
-$(document).ready(function() {
-  $('#navbar-login').on("click", function(){
-    event.preventDefault();
-    $(this).hide();
+// $(document).ready(function() {
+//   $('#navbar-login').on("click", function(){
+//     event.preventDefault();
+//     $(this).hide();
 
-    alert("Like a boss!");
-  });
+//     alert("Like a boss!");
+//   });
 
-  $('.error-messages').hover(function(){
-    $('.error-messages ul li').text("Melt Your Face!!!")
-  });
+//   $('.error-messages').hover(function(){
+//     $('.error-messages ul li').text("Melt Your Face!!!")
+//   });
 
-  $('.error-messages').on("click", function(){
-  $(this).hide();
-    alert("Like a boss!");
-  });
+//   $('.error-messages').on("click", function(){
+//   $(this).hide();
+//     alert("Like a boss!");
+//   });
 
-});
+// });
