f179a6fa80dae7bb4057a7835dfa205b3e4dae0a
Devin Wieland <devinwieland@gmail.com>
2014-04-13 17:49:01 -0500

    adjusted responses and controllers to conform with integer values in seedfile

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,40 +3,40 @@ class HomeController < ApplicationController
   def index
     @user = current_user
     @all_responses = Response.all.count
-    @panic = Response.where(answer: "panic").count
-    @panic_edge = Response.where(answer: "panic edge").count
-    @learning = Response.where(answer: "learning").count
-    @comfort_edge = Response.where(answer: "comfort edge").count
-    @comfort = Response.where(answer: "comfort").count
+    @panic = Response.where(answer: 1).count
+    @panic_edge = Response.where(answer: 2).count
+    @learning = Response.where(answer: 3).count
+    @comfort_edge = Response.where(answer: 4).count
+    @comfort = Response.where(answer: 5).count
   end
 
   def today
   	@all_responses = Response.where(created_at: (Date.yesterday..Date.today)).count
-  	@panic = Response.where(answer: "panic", created_at: (Date.yesterday..Date.today)).count
-    @panic_edge = Response.where(answer: "panic edge", created_at: (Date.yesterday..Date.today)).count
-    @learning = Response.where(answer: "learning", created_at: (Date.yesterday..Date.today)).count
-    @comfort_edge = Response.where(answer: "comfort edge", created_at: (Date.yesterday..Date.today)).count
-    @comfort = Response.where(answer: "comfort", created_at: (Date.yesterday..Date.today)).count
+  	@panic = Response.where(answer: 1, created_at: (Date.yesterday..Date.today)).count
+    @panic_edge = Response.where(answer: 2, created_at: (Date.yesterday..Date.today)).count
+    @learning = Response.where(answer: 3, created_at: (Date.yesterday..Date.today)).count
+    @comfort_edge = Response.where(answer: 4, created_at: (Date.yesterday..Date.today)).count
+    @comfort = Response.where(answer: 5, created_at: (Date.yesterday..Date.today)).count
   	render 'index'
   end
 
   def yesterday
   	@all_responses = Response.where(created_at: (Date.yesterday-1.day..Date.yesterday)).count
-  	@panic = Response.where(answer: "panic", created_at: (Date.yesterday-1.day..Date.yesterday)).count
-    @panic_edge = Response.where(answer: "panic edge", created_at: (Date.yesterday-1.day..Date.yesterday)).count
-    @learning = Response.where(answer: "learning", created_at: (Date.yesterday-1.day..Date.yesterday)).count
-    @comfort_edge = Response.where(answer: "comfort edge", created_at: (Date.yesterday-1.day..Date.yesterday)).count
-    @comfort = Response.where(answer: "comfort", created_at: (Date.yesterday-1.day..Date.yesterday)).count
+  	@panic = Response.where(answer: 1, created_at: (Date.yesterday-1.day..Date.yesterday)).count
+    @panic_edge = Response.where(answer: 2, created_at: (Date.yesterday-1.day..Date.yesterday)).count
+    @learning = Response.where(answer: 3, created_at: (Date.yesterday-1.day..Date.yesterday)).count
+    @comfort_edge = Response.where(answer: 4, created_at: (Date.yesterday-1.day..Date.yesterday)).count
+    @comfort = Response.where(answer: 5, created_at: (Date.yesterday-1.day..Date.yesterday)).count
   	render 'index'
   end
 
   def this_week
   	@all_responses = Response.where(created_at: (Date.yesterday-7.day..Date.yesterday)).count
-  	@panic = Response.where(answer: "panic", created_at: (Date.yesterday-7.day..Date.yesterday)).count
-    @panic_edge = Response.where(answer: "panic edge", created_at: (Date.yesterday-7.day..Date.yesterday)).count
-    @learning = Response.where(answer: "learning", created_at: (Date.yesterday-7.day..Date.yesterday)).count
-    @comfort_edge = Response.where(answer: "comfort edge", created_at: (Date.yesterday-7.day..Date.yesterday)).count
-    @comfort = Response.where(answer: "comfort", created_at: (Date.yesterday-7.day..Date.yesterday)).count
+  	@panic = Response.where(answer: 1, created_at: (Date.yesterday-7.day..Date.yesterday)).count
+    @panic_edge = Response.where(answer: 2, created_at: (Date.yesterday-7.day..Date.yesterday)).count
+    @learning = Response.where(answer: 3, created_at: (Date.yesterday-7.day..Date.yesterday)).count
+    @comfort_edge = Response.where(answer: 4, created_at: (Date.yesterday-7.day..Date.yesterday)).count
+    @comfort = Response.where(answer: 5, created_at: (Date.yesterday-7.day..Date.yesterday)).count
   	render 'index'
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20140410001602) do
   end
 
   create_table "responses", force: true do |t|
-    t.string   "answer"
+    t.integer  "answer"
     t.integer  "question_id"
     t.datetime "created_at"
     t.datetime "updated_at"
