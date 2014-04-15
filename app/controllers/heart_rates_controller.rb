b8fbdd3eb742d70ac66cb5049fbb89fb5a5ff99c
Apprentice <apprentice@dbc-chicago-003.local>
2014-04-14 22:39:09 -0500

    wtf

--- app/controllers/heart_rates_controller.rb
+++ app/controllers/heart_rates_controller.rb
@@ -1,7 +1,8 @@
 class HeartRatesController < ApplicationController
   def create
     @data = params
-    @heart_rate = HeartRate.create(bpm: params[:bpm], recorded_at: Datetime.strptime(params[:recorded_at],s%))
+    puts "#{params}"
+    @heart_rate = HeartRate.create(bpm: params[:bpm])#, #recorded_at: Datetime.strptime(params[:recorded_at],s%))
     redirect_to root_path
   end
 
