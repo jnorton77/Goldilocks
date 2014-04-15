5dfbb052619091a705b80f51fc1a381c89da22b7
Apprentice <apprentice@dbc-chicago-003.local>
2014-04-14 22:19:26 -0500

    on the verge of greatness

--- app/controllers/heart_rates_controller.rb
+++ app/controllers/heart_rates_controller.rb
@@ -1,6 +1,11 @@
 class HeartRatesController < ApplicationController
-  def mobile_create
-    @heart_rate = HeartRate.create(bpm: params[:bpm], recorded_at: params[:recorded_at])
+  def create
+    @data = params
+    @heart_rate = HeartRate.create(bpm: params[:bpm], recorded_at: Datetime.strptime(params[:recorded_at],s%))
     redirect_to root_path
   end
+
+
+
+
 end
