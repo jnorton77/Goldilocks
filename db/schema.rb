3f772d5a6237e85c10ce1cf1515603dcc7f8918e
Devin Wieland <devinwieland@gmail.com>
2014-04-15 14:33:20 -0500

    removed publics / assets

--- db/schema.rb
+++ db/schema.rb
@@ -33,7 +33,6 @@ ActiveRecord::Schema.define(version: 20140415032244) do
   create_table "responses", force: true do |t|
     t.integer  "answer"
     t.integer  "question_id"
-    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
