015757689bff950007e957edd0c32165132193fc
Devin Wieland <devinwieland@gmail.com>
2014-04-16 11:45:29 -0500

    rearranges home page with working css

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,18 +2,18 @@
 
 $(function() {
 
-    var quotes = $(".quotes");
-    var quoteIndex = -1;
+//     var quotes = $(".quotes");
+//     var quoteIndex = -1;
     
-    function showNextQuote() {
-        ++quoteIndex;
-        quotes.eq(quoteIndex % quotes.length)
-            .fadeIn(2000)
-            .delay(2000)
-            .fadeOut(2000, showNextQuote);
-    }
+//     function showNextQuote() {
+//         ++quoteIndex;
+//         quotes.eq(quoteIndex % quotes.length)
+//             .fadeIn(2000)
+//             .delay(2000)
+//             .fadeOut(2000, showNextQuote);
+//     }
     
-    showNextQuote();
+//     showNextQuote();
 
     HorizontalBarGraph = function(el, series) {
   this.el = d3.select(el);
